0f656708619ac19c2dc187f9a8ef95379681b480
Adriaen Limborgh <adriaen95@hotmail.com>
2024-06-13 12:40:59 +0200

    feat: Add Flask app with Triforce ASCII art
    This commit adds a Flask application that displays the Triforce ASCII art on the homepage. The ASCII art is rendered using a template and the Flask `render_template_string` function. The background color of the page is set to black, and the text color of the Triforce is animated using a CSS keyframe animation.
    
    The purpose of this commit is to create a simple Flask app to showcase the Triforce ASCII art and demonstrate the usage of Flask templates and CSS animations.

---
+++ main.py
@@ -0,0 +1,35 @@
+from flask import Flask, render_template_string
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    triforce = """
+   /\\
+  /__\\
+ /\\  /\\
+/__\\/__\\
+"""
+    return render_template_string('''
+    <html>
+        <head>
+            <style>
+                @keyframes pulse {
+                    0% { color: gold; }
+                    50% { color: white; }
+                    100% { color: gold; }
+                }
+                .triforce {
+                    animation: pulse 2s infinite;
+                }
+            </style>
+        </head>
+        <body style="background-color: black; font-family: monospace; text-align: center; padding-top: 50px;">
+            <h1 style="color: gold;">The Triforce</h1>
+            <pre class="triforce">{{ triforce }}</pre>
+        </body>
+    </html>
+    ''', triforce=triforce)
+
+if __name__ == '__main__':
+    app.run(debug=True)
